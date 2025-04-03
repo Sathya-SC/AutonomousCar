f299ab04adf2668afeb9e07a2fe303aa9acde759
Sathya <sct20037@uwu.ac.lk>
2025-04-03 12:09:34 +0530

    secondcommand

---
+++ build/CMakeFiles/rgbd_kinect.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/rgbd_kinect"
+  "../bin/rgbd_kinect.pdb"
+  "CMakeFiles/rgbd_kinect.dir/rgbd_kinect.cc.o"
+  "CMakeFiles/rgbd_kinect.dir/rgbd_kinect.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rgbd_kinect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sathya/ros2_ws/src/ORB_SLAM3/Examples/RGB-D
+# Build directory: /home/sathya/ros2_ws/src/ORB_SLAM3/Examples/RGB-D/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
